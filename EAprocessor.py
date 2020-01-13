663b18edcfa371dec1097a7de6e2bb4ec66755fd
szonglong <E0004621@u.nus.edu>
2020-01-13 18:47:39 +0800

    1.0.0
    First working: only plot export

--- EAprocessor.py
+++ EAprocessor.py
@@ -2,7 +2,7 @@
 ## EA data processor (Python 2.7)
 ## Function: EA Data Processor: Renames columns and does phase correction
 ## Author: Seah Zong Long
-## Version: 0.1.1
+## Version: 1.0.0
 ## Last modified: 13/01/2020
 
 ## Changelog: 
@@ -10,6 +10,7 @@
 ## 0.0.1 pandas-iefy
 ## 0.1 SSR fit, into df
 ## 0.1.1 Make into plot
+## 1.0.0 Export processed plots
 
 ## Instructions:
 ## Similar to pytest. Open and select file dir that contains EA files
@@ -48,7 +49,8 @@ wlist=[] #working list
 for filename in (filelist):
     if 'Vdc' in filename:
         wlist.append(filename)
-        
+print wlist        
+first = 0
 
 for filename in wlist:
 
@@ -58,14 +60,16 @@ for filename in wlist:
 
 ### Small subroutine to find the voltage used from the filename ###    
     voltage_name = ''
-    for i in range(len(filename)):
-        if filename[i] == '-':
-            i2 = filename.index('V')
-            voltage_name = filename[i:i2]
-        elif filename[i] == 'V':
-            if filename[i-2] == '.':
-                voltage_name = filename[i-3:i]
-                    
+    
+    if '-' in filename:
+        voltage_name = filename[filename.index('-'):filename.index('V')]
+    else:
+        if '.' in filename:
+            voltage_name = filename[filename.index('.')-1:filename.index('V')]
+        else:
+            voltage_name = filename[filename.index('V')-1:filename.index('V')]
+    
+#    print voltage_name    
 
     for line in lines:
         split_line = [float(i) for i in line.split()]
@@ -83,8 +87,19 @@ for filename in wlist:
         split_line[11]=split_line[8]*-np.sin(res.x)+split_line[9]*np.cos(res.x)
 
     df = pd.DataFrame.from_records(split_lines,columns=['PE','Vdc','Vpd','Vpd_SD','Ch1','Ch1_SD','Ch2','Ch2_SD','Ch1/Vpd','Ch2/Vpd','%s' %voltage_name,'Ch2p','m'])
-    fig, axes = plt.subplots(sharex=True)
-    df.plot(x='PE',y=['%s' %voltage_name,'Ch2p'])
-#    plt.savefig('processed_semilog/%s _p.png' %file)
+#    fig, axes = plt.subplots(sharex=True)
+    least_sq_plot = df.plot(x='PE',y=['%s' %voltage_name,'Ch2p']).get_figure()
+    if first == 0:
+        big_plot_array=(df.PE.to_frame()).join(df['%s' %voltage_name])
+        first = 1
+        
+    else:
+        big_plot_array=big_plot_array.join(df['%s' %voltage_name])
+        
+    least_sq_plot.savefig('processed/%s _p.png' %voltage_name)
+    plt.close()
 #    plt.close()
-    
+    f.close()
+big_plot = big_plot_array.plot(x='PE',legend).get_figure()
+big_plot.savefig('processed/bigplot.png')
+plt.close()
